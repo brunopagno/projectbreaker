ebbefdc6d4168ccf5f93f4a6c46e5ff1b5e0e75e
Bruno Pagno <brunopagno@gmail.com>
2018-10-01 23:27:11 -0300

    Added horizontal movement

---
+++ Assets/Src/HorizontalMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalMovement : MonoBehaviour
+{
+    public float speed;
+
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        gameObject.transform.position += Vector3.right * horizontalInput * speed * Time.deltaTime;
+    }
+}

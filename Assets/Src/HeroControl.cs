8530ffb5c4c40a732623b0d2be74dc0718f1e2a3
Bruno Pagno <brunopagno@gmail.com>
2018-10-03 22:29:12 -0300

    Did a lot of things

---
+++ Assets/Src/HeroControl.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+
+public class HeroControl : MonoBehaviour
+{
+    public float speed = 2;
+    public float acceleration = 200;
+    public float jump = 450;
+    public Rigidbody body;
+
+    private bool isJumping = false;
+
+    private void FixedUpdate()
+    {
+        ResolveRun();
+        ResolveJump();
+    }
+
+    private void ResolveRun()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        body.AddForce(Vector3.right * horizontalInput * acceleration);
+        if (Mathf.Abs(body.velocity.x) > speed)
+        {
+            body.velocity = body.velocity.normalized * speed;
+        }
+    }
+
+    private void ResolveJump()
+    {
+        if (!isJumping && Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+            body.velocity = new Vector3(body.velocity.x, 0, body.velocity.z);
+            body.AddForce(Vector3.up * jump);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        isJumping = false;
+    }
+}

d9503f139c2d9e448e0a712f90138dfcbac53131
Bruno Pagno <brunopagno@gmail.com>
2018-10-14 11:38:42 -0300

    Partial updates

--- Assets/Src/HeroControl.cs
+++ Assets/Src/HeroControl.cs
@@ -1,19 +1,32 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class HeroControl : MonoBehaviour
 {
+    public Rigidbody body;
+
     public float speed = 2;
     public float acceleration = 200;
     public float jump = 450;
-    public Rigidbody body;
+    public float fireRate = 4;
 
-    private bool isJumping = false;
+    public GameObject projectile;
+    public Vector3 projectileOrigin;
+
+    private bool _isJumping = false;
+    private float _timeBetweenShoots;
+    private float _timeToShoot;
+
+    private void Start()
+    {
+        _timeBetweenShoots = fireRate / 60.0f;
+    }
 
     private void FixedUpdate()
     {
         ResolveRun();
         ResolveJump();
+        ResolveShoot();
+        //ResolveBreak();
     }
 
     private void ResolveRun()
@@ -29,16 +42,44 @@ public class HeroControl : MonoBehaviour
 
     private void ResolveJump()
     {
-        if (!isJumping && Input.GetButtonDown("Jump"))
+        if (!_isJumping && Input.GetButtonDown("Jump"))
         {
-            isJumping = true;
+            _isJumping = true;
             body.velocity = new Vector3(body.velocity.x, 0, body.velocity.z);
             body.AddForce(Vector3.up * jump);
         }
     }
 
+    private void ResolveShoot()
+    {
+        _timeToShoot += Time.deltaTime;
+        if (Input.GetButton("Fire1") && _timeToShoot > _timeBetweenShoots)
+        {
+            Instantiate(projectile, transform.position + projectileOrigin, Quaternion.identity);
+            _timeToShoot = 0;
+        }
+    }
+
+    //private void ResolveBreak()
+    //{
+    //    // nothing here, yet
+    //}
+
     private void OnTriggerEnter(Collider other)
     {
-        isJumping = false;
+        _isJumping = false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        float size = 0.2f;
+        Gizmos.DrawLine(
+            transform.position + projectileOrigin + new Vector3(0, -size, 0),
+            transform.position + projectileOrigin + new Vector3(0, size, 0)
+            );
+        Gizmos.DrawLine(
+            transform.position + projectileOrigin + new Vector3(-size, 0, 0),
+            transform.position + projectileOrigin + new Vector3(size, 0, 0));
     }
 }
---
+++ Assets/Src/ProjectileBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ProjectileBehaviour : MonoBehaviour
+{
+    public float speed = 8;
+
+    private float _limit = 40;
+
+    private void Update()
+    {
+        transform.position += Vector3.right * Time.deltaTime * speed;
+
+        if (Mathf.Abs(transform.position.x) > _limit)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
